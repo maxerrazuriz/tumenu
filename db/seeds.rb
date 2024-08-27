6ba665ec04bb3e70ed2137e137d3c22df4a174a3
Max Errazuriz <maxerrazurizc@gmail.com>
2024-08-27 11:54:09 +0200

    norika asked to push this

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+CUISINE = ["japanese", "canadian", "spanish", "italian", "german", cuban]
+User.destroy_all
+users = []
+meals = []
+ingrs = []
+5.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Locations::Australia.location,
+    age: rand(7..38),
+    email: Faker::Internet.email,
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+5.times do
+  meal = Meal.new(
+    cuisine: CUISINE.sample,
+    description: Faker::GreekPhilosophers.quote,
+    recipe: Faker::Quote.famous_last_words,
+    user: users.sample
+  )
+end
+
+5.times do
+  ingr = Ingridient.new(
+    name: Faker::Food.ingredient,
+    unit: ["l", "ml", "kg", "g", "ly"].sample
+  )
+end
