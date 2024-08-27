17474d68c4f940789a42570cc98d9301b5dfacc1
Max Errazuriz <maxerrazurizc@gmail.com>
2024-08-27 18:18:01 +0200

    seachbar half completed

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,48 @@
 class MealsController < ApplicationController
-
   def index
     @meal = Meal.all
+    carousel
+    if params[:query].present?
+      sql_subquery = <<~SQL
+        name @@ :query
+        OR description @@ :query
+        OR meal_ingredients.ingredients @@ :query
+      SQL
+      @meal = @meal.joins(:name).where(sql_subquery, query: "%#{params[:query]}%")
+    end
+
   end
 
+  def new
+    @meal = Meal.new
+  end
+
+  def show
+    @meal = Meal.find(params[:id])
+    @ingredients = @meal.meal_ingredients
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    @meal.user = current_user
+    if @meal.save
+      redirect_to meal_path(@meal)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def carousel
+    @japanese_list = Meal.where(cuisine: "japanese")
+    @indian_list = Meal.where(cuisine: "indian")
+    @italian_list = Meal.where(cuisine: "italian")
+    @canadian_list = Meal.where(cuisine: "canadian")
+    @meals = [@japanese_list, @indian_list, @italian_list, @canadian_list]
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :cuisine, :description, :recipe, :user) #then add picture
+  end
 end
