579b07f53f76618e42874e40df9b7067443a083f
Norika Niyungeko <norikaniyungeko@gmail.com>
2024-08-27 12:13:28 +0200

    added columns to meal and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_26_152621) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_27_101214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_26_152621) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_meals_on_user_id"
   end
 
@@ -95,6 +96,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_26_152621) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
