9d3201d594fa87753db139b54cc311aa465858ac
Max Errazuriz <maxerrazurizc@gmail.com>
2024-08-27 11:01:50 +0200

    controllers added

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,6 @@ class Meal < ApplicationRecord
   belongs_to :user
   has_many :ingridients, through: :meal_ingredients
   has_many :reviews
+
   validates :cuisine, :description, :recipe, presence: true
 end
